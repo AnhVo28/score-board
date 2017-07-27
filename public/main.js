da346f9aa637ddf8a3696fd8b5f18113b65f9c96
Hoang Anh Vo <vohoanganhkhtn@gmail.com>
2017-07-27 11:31:13 +0300

    decomposing application

--- public/main.js
+++ public/main.js
@@ -1,24 +1,43 @@
+function Header(props) {
+  return(
+    <div className='header'>
+      <h1>Scoreboard</h1>
+    </div>
+  )
+}
+
+function Player(props) {
+  return(
+    <div className='player'>
+      <div className='player-name'>
+        {props.name}
+      </div>
+      <div className='player-score'>
+        <Counter score ={props.score}/>
+      </div>
+    </div>
+  )
+}
+
+function Counter(props) {
+  return(
+    <div className='counter'>
+      <button className='counter-action decrement'> - </button>
+      <div className='counter-score' >{props.score}</div>
+      <button className='counter-action increment'> + </button>
+    </div>
+  )
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className='scoreboard'>
-        <div className='header'>
-          <h1>Scoreboard</h1>
-        </div>
-        <div className='players'>
-          <div className='player'>
-            <div className='player-name'>
-              Jim Hoskins
-            </div>
-            <div className='player-score'>
-              <div className='counter'>
-                <button className='counter-action decrement'> - </button>
-                <div className='counter-score' >32</div>
-                <button className='counter-action increment'> + </button>
-              </div>
-            </div>
+        <Header/>
+          <div className='players'>
+            <Player name ='John Son' score ={32}/>
+            <Player name ='Jim Hoskin' score ={10}/>
           </div>
-        </div>
       </div>
     );
   }
